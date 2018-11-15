7c29d50240e713c28942dee456000cfbe4757fb3
georgevlad42 <enevlad3000@yahoo.com>
2018-11-15 19:50:28 +0200

    Tema 2 AOS

--- lab2aos-hw/src/main/java/com/unitbv/mi/lab2aoshw/functionality/HelloFunction.java
+++ lab2aos-hw/src/main/java/com/unitbv/mi/lab2aoshw/functionality/HelloFunction.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/docker/hello")
+@RequestMapping("/helloFunction")
 public class HelloFunction {
 
     @GetMapping
